8ba90155907888fe2084976df57bed9ec2adac53
Joseph T Lyons <JosephTLyons@gmail.com>
2020-08-24 23:09:13 -0400

    More katas

---
+++ uniq.py
@@ -0,0 +1,16 @@
+# Implement a function which behaves like the uniq command in UNIX.
+
+# It takes as input a sequence and returns a sequence in which all duplicate elements following each other have been reduced to one instance.
+
+# Example:
+#     ["a", "a", "b", "b", "c", "a", "b", "c"]  =>  ["a", "b", "c", "a", "b", "c"]
+
+
+def uniq(seq):
+    characters = []
+
+    for item in seq:
+        if len(characters) == 0 or characters[-1] != item:
+            characters.append(item)
+
+    return characters

ab197e7593b1f213bca094034ddbac344ed1a7f9
Joseph T Lyons <JosephTLyons@gmail.com>
2020-09-01 22:34:10 -0400

    Update formatting

--- create_phone_number.py
+++ create_phone_number.py
@@ -4,7 +4,7 @@
 # those numbers in the form of a phone number.
 
 # Example:
-#     create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
 
 # The returned format must be correct in order to complete this challenge.
 # Don't forget the space after the closing parentheses!
--- list_filtering.py
+++ list_filtering.py
@@ -2,9 +2,9 @@
 # returns a new list with the strings filtered out.
 
 # Example
-#     filter_list([1,2,'a','b']) == [1,2]
-#     filter_list([1,'a','b',0,15]) == [1,0,15]
-#     filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 
 def filter_list(l):

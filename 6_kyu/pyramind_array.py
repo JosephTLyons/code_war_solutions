888609e0ba75b8d01c7378045155eb16ba98516b
Joseph T Lyons <JosephTLyons@gmail.com>
2020-10-26 21:50:53 -0400

    Create pyramind_array.py

---
+++ 6_kyu/pyramind_array.py
@@ -0,0 +1,11 @@
+# Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+# pyramid(0) => [ ]
+# pyramid(1) => [ [1] ]
+# pyramid(2) => [ [1], [1, 1] ]
+# pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+# Note: the subarrays should be filled with 1s
+
+
+def pyramid(n):
+    return [[1 for _ in range(i + 1)] for i in range(n)]
